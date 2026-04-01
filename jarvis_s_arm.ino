a54c3c1046ce38b12c8c83b5399a76e26d843c22
gok24code <noxonninen@gmail.com>
2026-04-01 23:49:58 +0300

    Refactor servo definitions and pin assignments for clarity

--- jarvis_s_arm.ino
+++ jarvis_s_arm.ino
@@ -1,15 +1,20 @@
 //comment: servo definitons.
 
 #include <Servo.h>
-Servo base, shoulder, wrist, elbow1, elbow2, grip;
+Servo base;
+Servo shoulder;
+Servo wrist;
+Servo elbow1;
+Servo elbow2;
+Servo grip;
 struct Pins {
 
-  int b = 13;
-  int s = 12;
-  int w = 11;
-  int e1 = 10;
-  int e2 = 9;
-  int g = 8;
+  int b = 9;
+  int s = 10;
+  int w = 8;
+  int e1 = 11;
+  int e2 = 12;
+  int g = 13;
 };
 Pins pins;
 
@@ -23,9 +28,13 @@ void setup() {
   elbow2.attach(pins.e2);
   grip.attach(pins.g);
 
-  base.write(45);
-  delay(300);
-  base.write(135);
+  base.write(90); // 0-180 half chamber 90 angel look forward
+  shoulder.write(160); //0-180 half-cycle ideal angle 160- for normal 70 for uzanma 
+  wrist.write(90); // uzanması 120 normali 90
+  elbow1.write(75); // normal 75 0-180 yukarı aşağı angle serbest
+  elbow2.write(180); // 0-180 speed turn 
+  grip.write(180); // 150- open 180- closed
+
 
 }
 
